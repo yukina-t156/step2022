4fe12f740e3c1fde139e58c1ac9e59eb5c192753
立田結希菜 <yukinatatsuta@TatsutaYukinanoMacBook-Pro.local>
2022-05-23 01:18:22 +0900

    add homework3 進捗

---
+++ homework3/homework3_1.py
@@ -0,0 +1,163 @@
+#! /usr/bin/python3
+
+from audioop import maxpp
+from lib2to3.pgen2 import token
+
+
+def read_number(line, index):
+    number = 0
+    while index < len(line) and line[index].isdigit():
+        number = number * 10 + int(line[index])
+        index += 1
+    if index < len(line) and line[index] == '.':
+        index += 1
+        decimal = 0.1
+        while index < len(line) and line[index].isdigit():
+            number += int(line[index]) * decimal
+            decimal /= 10
+            index += 1
+    token = {'type': 'NUMBER', 'number': number}
+    return token, index
+
+
+def read_plus(line, index, last_priority):  # 一つ前のpriorityを追加
+    token = {'type': 'PLUS'}
+    return token, index + 1, 0  # 優先度は0
+
+
+def read_minus(line, index, last_priority):
+    token = {'type': 'MINUS'}
+    return token, index + 1, 0  # 優先度は0
+
+
+def read_asterisk(line, index, last_priority):  # 追加 *を読み込む
+    token = {'type': 'ASTERISK'}
+    priority = 1
+    if last_priority > 0:
+        priority = last_priority
+    return token, index + 1, priority  # 前が+,-なら+1,前が*,/なら増減0
+
+
+def read_slash(line, index, last_priority):  # 追加 /を読み込む
+    token = {'type': 'SLASH'}
+    priority = 1
+    if last_priority > 0:
+        priority = last_priority
+    return token, index + 1, priority  # 前が+,-なら+1,前が*,/なら増減0
+
+
+def tokenize(line):
+    tokens = []
+    priorities = [0]  # 追加 最初にダミー分の初期値0を入れておく
+
+    index = 0
+    while index < len(line):
+        if line[index].isdigit():
+            (token, index) = read_number(line, index)
+        # 以下に1つ前のprioritiesからの増減を表すpriorityを追加
+        else:  # typeの時
+            if line[index] == '+':
+                (token, index, priority) = read_plus(
+                    line, index, priorities[-1])
+            elif line[index] == '-':
+                (token, index, priority) = read_minus(
+                    line, index, priorities[-1])
+            elif line[index] == '*':
+                (token, index, priority) = read_asterisk(
+                    line, index, priorities[-1])
+            elif line[index] == '/':
+                (token, index, priority) = read_slash(
+                    line, index, priorities[-1])
+            else:
+                print('Invalid character found: ' + line[index])
+                exit(1)
+            priorities.append(priority)  # 'type'の時のみprioritiesを更新し追加
+        tokens.append(token)
+    return tokens, priorities  # priorityを返り値に追加
+
+
+def bin_operation(token_type, num1, num2):  # 二項演算をして値を返す関数
+    if token_type == 'PLUS':
+        return num1+num2
+    elif token_type == 'MINUS':
+        return num1-num2
+    elif token_type == 'ASTERISK':
+        return num1*num2
+    elif token_type == 'SLASH':
+        return num1/num2
+    else:
+        print('Invalid syntax')
+        exit(1)
+
+
+def evaluate(tokens, priorities):  # prioritiesを受け取る
+    answer = 0
+    max_priority = max(priorities)  # 優先度の最大値を取得
+    tokens.insert(0, {'type': 'PLUS'})  # Insert a dummy '+' token
+    if tokens[-1]['type'] != 'NUMBER':  # 最後のtokensのみおかしい時に弾く
+        print('Invalid syntax')
+        exit(1)
+    while max_priority > 0:  # max_priorityが0になるまで処理
+        index = 1  # 1つ目の数字 毎ループ初期化するのでこの位置に
+        while index < len(tokens):
+            if tokens[index]['type'] == 'NUMBER':  # 数字が来た時に演算を実行
+                if priorities[int(index/2)] == max_priority:
+                    if tokens[index-2]['type'] == 'NUMBER':
+                        num = bin_operation(
+                            tokens[index - 1]['type'], tokens[index-2]['number'], tokens[index]['number'])  # 演算結果
+                        tokens[index-2] = {'type': 'NUMBER',
+                                           'number': num}  # 組にして入れる
+                        del tokens[index-1:index+1]  # tokensの更新
+                        del priorities[int(index/2)]  # prioritiesの更新
+                        index -= 3  # 削除分減らす(3文字減らして1文字増やす)
+                    else:
+                        print('Invalid syntax')
+                        exit(1)
+                elif tokens[index-1]['type'] not in ['PLUS', 'MINUS', 'ASTERISK', 'SLASH']: # エラー処理のため
+                    print('Invalid syntax')
+                    exit(1)
+            index += 1
+        max_priority -= 1
+
+    # priority==0の時のみansを更新する
+    index = 1  # 初期化
+    while index < len(tokens):
+        if tokens[index]['type'] == 'NUMBER':  # 数字が来た時に演算を実行
+            if tokens[index-1]['type'] in ['PLUS', 'MINUS', 'ASTERISK', 'SLASH']:  # エラー処理のため
+                # answerとの演算結果をanswerに代入
+                answer = bin_operation(
+                    tokens[index-1]['type'], answer, tokens[index]['number'])
+            else:
+                print('Invalid syntax')
+                exit(1)
+        index += 1
+    return answer
+
+
+def test(line):
+    (tokens, priorities) = tokenize(line)
+    actual_answer = evaluate(tokens, priorities)
+    expected_answer = eval(line)
+    if abs(actual_answer - expected_answer) < 1e-8:
+        print("PASS! (%s = %f)" % (line, expected_answer))
+    else:
+        print("FAIL! (%s should be %f but was %f)" %
+              (line, expected_answer, actual_answer))
+
+
+# Add more tests to this function :)
+def run_test():
+    print("==== Test started! ====")
+    test("1+2")
+    test("1.0+2.1-3")
+    print("==== Test finished! ====\n")
+
+
+run_test()
+
+while True:
+    print('> ', end="")
+    line = input()
+    (tokens, priorities) = tokenize(line)  # 追加 priorityを定義
+    answer = evaluate(tokens, priorities)
+    print("answer = %f\n" % answer)
---
+++ homework3/homework3_document.md
@@ -0,0 +1,130 @@
+# 宿題1
+## 方針
+- スタックを使ってもできそうだが、なるべくソースコードの原型を残したいので今回は使わない。（うまく組み合わせられる自信がない） 
+- 優先度を追加し、*,/の時に優先度を高くしておく。 
+- 優先度の最大値を保持し、その回数式を更新してループにかければできる？ 
+- 式を更新する際にpriorities,tokensで使った部分を切り取るのを忘れない
+- evaluate部分について
+```
+例:1*4/2+4*2+1のとき
+tokens[+,1,*,4,/,2,+,4,*,2,+,1]
+priorities[0,1,1,0,1,0]
+
+index=3の時
+
+#max_priorityの時のみ実行
+if max_priority==priorities[(int)index/2]: 
+    tokens[index-2]=(1*4) #1つ目の数字を置き換える
+    tokens[index-1:index+1].erase #tokensの演算子と2つ目の数字を削除
+    priorities[int(index/2)].erase #該当するpriorityを削除
+
+結果:
+tokens[+,4,/,2,+,4,*,2,+,1]
+priorities[0,1,0,1,0]
+(=4/2+4*2+1)
+```
+のような感じにできそう?  
+
+
+
+## 手順1
+read_asterisk と read_slashをdefし、tokenize内にも追加  
+※evaluate内には手順3で追加
+
+## 手順2
+read_関数内,tokenize,メインのwhile文にpriorities,priority,last_priorityを追加
+※tokenizeの戻り値を(tokens,priorities)にしておく  
+※read_の返り値を(token,index+1,priority)にする  
+
+## 手順3  
+*,/,及びpriorityの処理をevaluateに追加  
+typeと2つの数を受け取り結果を返す二項演算の関数を作る:binary_operation  
+priority==0の時のみansに足し引きする
+
+- 挿入するときなどにtypeとのtupleにすることを忘れない！(気づかずにエラーを吐かせてしまった)
+
+***
+# 宿題2 //未
+testcaseをruncaseに追加  
+できるだけ網羅的に
+
+***
+# 宿題3 //未
+(,)の分もtoken,prioritiesに保存する(priorityの受け渡しができないので)
+evaluateで出てきた時に何もせずprioritiesとtokensから削除する
+- *,/,(の優先度を上げる  
+  - *,/の時
+    - 前が+,-,*,/の時は宿題1と同じ
+    - 前が(の時は(+1
+    - 前が)の時は)と同じ
+  - (なら直前のpriority+1  
+  - )なら演算子の優先度を下げる
+    - この時にその前に出てきた(の前まで下げるので、保持しておく必要がある。
+    - (が出てきたら直前の優先度をスタックに入れ、)が出るたびにpopして使う
+- 宿題1と同様に優先度のMaxを保存、優先度が高いものから行う
+例
+1+2*3  
+0 1 1  
+ ( +  ) 1 * 3 * ( 3 + 1 * 4 ) - 3  
+　0　1　1　2　2　3　1　0  
+
+## 手順1
+- 'type': 'PAREN'を追加
+- read_leftparen、read_rightparenを作成
+ - stackをtokenizeに追加し引数、返り値に追加
+- tokenizeに'(',')'の分岐を追加
+- 左右の区別は優先度のみでOK
+スタックの実装
+```
+from collections import deque #スタックに必要
+
+#tokenize内
+left_st = deque([]) #スタックを用意
+
+#read_left_paren内
+def read_left_paren(line, index, last_priority, left_st):
+  left_st.append(last_priority)
+  # 略
+  return token, index + 1, priority, left_st
+
+
+#read_right_paren内
+def read_right_paren(line, index, last_priority, left_st):
+  priority = left_st.pop()
+  # 略
+  return token, index + 1, priority, left_st
+
+```
+### +,-,*,/も宿題1のままだと動かない！
+例: 1+(1+3*4)*2  
+-> （内の最低値の基準のようなものを決めておく  
+例えば  
+1+(1-1-1-1-1-1-1)+1  
+ 01 0 0 0 0 0 0 00 のようにならないように、下限を決めて  
+1+(1-1-1-1-1-1-1)+1  
+ 01 1 1 1 1 1 1 00  
+1+(1+(1+1+1)+1)+1  
+ 01 12 2 2 11 00  
+ のようになるように  
+- ベースの変数を作り、'('と')'で更新し常にpriorityに足すようにする  
+- read_内で入れなくてもtokenize内だけで処理できそう？  
+-  初期のbaseは0に  
+-  -10とかにならないように last_priorityにはbaseで揃える前のものを渡す(-baseにすれば良い)
+
+## 手順2
+read_asterisk, read_slash内のpriorityの処理を変更
+- 前が'('だったら1になるパラメータleft_paramをやむなく追加  
+(あまり変なパラメータを作りたくないが、tokensを全て渡すよりは良いかと思った)  
+'(('のように連続するケースでも大丈夫
+  - *,/の時
+    - 前が+,-の時は+1
+    - 前が*,/の時は宿題1と同じ
+    - 前が(の時は(+1 
+    - 前が)の時は)と同じ
+
+## 手順３
+
+## メモ  
+read_内のpriorityを変更させる部分も関数にできそう  
+変数の付け方上手になりたい!  
+[https://qiita.com/Ted-HM/items/7dde25dcffae4cdc7923]とかリーダブルコードとかちゃんと読む
